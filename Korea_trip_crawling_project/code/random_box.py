9e2e831b404b02d50716823e5294ff3c0f52d207
개발하는 김곰돌ᕕʕ •ₒ• ʔ୨ <rbgmll10331@gmail.com>
2022-05-16 16:34:03 +0900

    코드 수정

--- Korea_trip_crawling_project/code/random_box.py
+++ Korea_trip_crawling_project/code/random_box.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import *
 import random
 
@@ -43,27 +44,27 @@ def rand_choice() :
 
 window = Tk()
 window.title("랜덤 추첨")
-window.config(padx=10, pady=10, bg="lightblue")
+window.config(padx=10, pady=10, bg="ivory")
 
-canvas = Canvas(window, height=680, width=680, bg="ivory")
+canvas = Canvas(window, height=600, width=600, bg="ivory")
 canvas.pack()
 
-img_main = PhotoImage(file="images/gift.png")
-canvas_img = canvas.create_image(340, 340, image=img_main)
-canvas.create_text(340, 30, text="랜덤박스", fill="brown", \
-    font=("나눔바른펜", 30, "bold"))
+img_main = tkinter.PhotoImage(file="images/gift.png", master=window)
+canvas_img = canvas.create_image(300, 300, image=img_main)
+# canvas.create_text(340, 30, text="랜덤박스", fill="brown", \
+#    font=("나눔바른펜", 30, "bold"))
 
 
 img_list = []
-for i in range(9) :
-    img = PhotoImage(file=f"area/{i+1}.jpg")
+for i in range(6) :
+    img = tkinter.PhotoImage(file=f"images/img{i}.png", master=window)
     img_list.append(img)
 
-button = Button(window, text="랜덤 뽑기", bg="white", fg="hotpink", \
-    font=("나눔바른펜", 20, "bold"), \
+button = Button(window, text="여행지 뽑기", bg="white", fg="orange", \
+    font=("나눔바른펜", 17, "bold"), \
     command=rand_choice)
 
-button.place(x=550, y=0)
+button.place(x=470, y=0)
 
 
 window.mainloop()
